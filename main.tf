ce19e3996952fb47823fad06cb9425ec300a5177
Esneider23 <guzmane@utb.edu.co>
2023-11-01 10:42:28 -0500

    update

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.28.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "backup-terraform"
+    storage_account_name = "terraformstateproyect"
+    container_name       = "tfstatesdevops"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
