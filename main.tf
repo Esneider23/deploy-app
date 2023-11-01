9d4fac166353e7c82b1cd612a3e959b6d3f64f40
Esneider23 <guzmane@utb.edu.co>
2023-11-01 12:25:49 -0500

    update

--- main.tf
+++ main.tf
@@ -23,3 +23,36 @@ resource "azurerm_resource_group" "rg_service_web" {
   name = "rg_service_web" # this is the name on azure
   location = "eastus" # data center location on azure
 }
+
+variable "imagebuild" {
+  type = string
+  description = "the latest image build version"
+}
+
+resource "azurerm_resource_group" "rg_utbapp" {
+  name = "rg_utbapp" # this is the name on azure
+  location = "eastus" # data center location on azure
+}
+
+resource "azurerm_container_group" "tf_cg_utb" {
+  name                  = "motorshop"
+  location              = azurerm_resource_group.rg_utbapp.location #utilising the resource group
+  resource_group_name   = azurerm_resource_group.rg_utbapp.name #utilising the resource group
+
+  ip_address_type       = "Public"
+  dns_name_label        = "MOTORSHOP" #friendly name we want to give our domain
+  os_type               = "Linux"
+
+  # Specify the container information
+  container {
+    name = "app-deploy"
+    image = "esneider23/app-deploy:${var.imagebuild}"
+    cpu = "1"
+    memory = "1"
+
+    ports {
+        port = 80
+        protocol = "TCP"
+    }
+  }
+}
