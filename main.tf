d19d4aec60fe44bd86433a9021d7b6dace9cdfa1
Esneider23 <guzmane@utb.edu.co>
2023-11-09 17:32:21 -0500

    update

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "azurerm_service_plan" "app" {
 resource "azurerm_linux_web_app" "app-motorshop" {
   name                = "app-motorshop"
   resource_group_name = azurerm_resource_group.rg_service_web.name
-  location            = azurerm_service_plan.rg_service_web.location
+  location            = azurerm_service_plan.app.location
   service_plan_id     = azurerm_service_plan.app.id
 
   site_config {
